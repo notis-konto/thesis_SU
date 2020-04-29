72f5f8c23f8b875f385c0f624196a690dabb01bc
notis <notis@DESKTOP-TD61130>
2020-04-29 21:35:06 +0200

    fix name

--- thesis_algorithm.py
+++ thesis_algorithm.py
@@ -184,5 +184,5 @@ while x < 51:
 			P1 = P1 + step
 		y=y+1
 	x=x+1
-write_xls("results.csv",results)
-write_xls("probabilities.csv",probabilities)
+write_xls("results",results)
+write_xls("probabilities",probabilities)
